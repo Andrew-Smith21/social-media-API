ba83055d58c828e55dc72cfacf42e523a8d5b72c
Chad Smith <chad.smith@vanderbilt.edu>
2022-08-21 23:14:27 -0400

    updated server file

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hun
   useUnifiedTopology: true
 });
 
-// Use this to log mongo queries being executed!
+// Log mongo queries
 mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
